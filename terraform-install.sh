491750f5d717baddbccd4ee9eaf176441b4a67a6
devopscalgary <93885659+devopscalgary@users.noreply.github.com>
2022-03-23 23:40:34 -0600

    Update terraform-install.sh

--- terraform-install.sh
+++ terraform-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # install terraform in ubuntu server
 sudo apt install wget unzip -y
-wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
-sudo unzip terraform_0.12.26_linux_amd64.zip -d /usr/local/bin/
+wget https://releases.hashicorp.com/terraform/1.1.7/terraform_1.1.7_linux_amd64.zip
+sudo unzip terraform_1.1.7_linux_amd64.zip -d /usr/local/bin/
 #Export terraform binary path temporally
 export PATH=$PATH:/usr/local/bin
 #Add path permanently for current user.By Exporting path in .bashrc file at end of file.
